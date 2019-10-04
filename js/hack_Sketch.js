9b2d2442bd8b4e194953ae241bd8bb142df77bf1
nekoshiro046 <nekoshiro046@gmail.com>
2019-10-04 15:59:24 +0900

    Updata

--- js/hack_Sketch.js
+++ js/hack_Sketch.js
@@ -1,3 +1,16 @@
+// loadingのdivを取得
+var loading = document.getElementById('loading');
+// contentsのdivを取得
+var contents = document.getElementById('wrapper');
+
+// 読み込みが完了したら
+window.addEventListener('load', function () {
+	// loadingのdivを非表示に
+	loading.style.display = 'none';
+	// contentsのdivを表示
+	contents.classList.remove('hidden');
+});
+
 var img,img2,img3,img4;
 var imgs = [];
 var canvas;
